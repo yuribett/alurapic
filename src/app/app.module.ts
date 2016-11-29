1a196454c55b62318f3e41401bc0a40034664333
Yuri Bett <yuri@dynamix.com.br>
2016-11-29 12:52:03 -0200

    Creating PhotoComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PhotoModule } from './photo/photo.module';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    PhotoModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]  
 })
 export class AppModule { }
---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'photo',
+  templateUrl: './photo.component.html',
+  styleUrls: ['./photo.component.css']
+})
+export class PhotoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
