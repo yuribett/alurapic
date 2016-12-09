9cebc4d992a2108f6d781748ea157a164d753ea2
Yuri Bett <yuribett@gmail.com>
2016-12-09 13:03:59 -0200

    Implementing photo service

--- src/app/photo/photo.module.ts
+++ src/app/photo/photo.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PhotoComponent } from './photo.component';
 import { FilterByTitle } from './photo.pipe';
+import { PhotoService } from './photo.service';
 
 @NgModule({
   declarations: [ PhotoComponent, FilterByTitle ],
-  exports: [ PhotoComponent, FilterByTitle ]
+  exports: [ PhotoComponent, FilterByTitle ],
+  providers: [ PhotoService ]
 })
 export class PhotoModule { }
